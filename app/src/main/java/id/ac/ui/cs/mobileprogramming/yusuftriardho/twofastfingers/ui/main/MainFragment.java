9249625865fd3bedda7a98f8c2760d946525c11f
rdo <rdo@rdos-MacBook-Pro.local>
2019-10-29 13:25:01 +0700

    add playing state

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/yusuftriardho/twofastfingers/ui/main/MainFragment.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/yusuftriardho/twofastfingers/ui/main/MainFragment.java
@@ -6,14 +6,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import id.ac.ui.cs.mobileprogramming.yusuftriardho.twofastfingers.R;
 
 public class MainFragment extends Fragment {
+
+    Button sideBarBtn, startBtn, timer, backBtn;
+    TextView title, textBox, resultBox;
+    EditText input;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -29,8 +37,8 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        getRes();
 
-        Button sideBarBtn = getView().findViewById(R.id.sidebar_btn);
         sideBarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,5 +48,77 @@ public class MainFragment extends Fragment {
                         .commit();
             }
         });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, MainFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStartClick(view);
+            }
+        });
     }
+
+    public void getRes() {
+        sideBarBtn = getView().findViewById(R.id.sidebar_btn);
+        startBtn = getView().findViewById(R.id.start_btn);
+        title = getView().findViewById(R.id.title);
+        textBox = getView().findViewById(R.id.textBox);
+        input = getView().findViewById(R.id.inputText);
+        timer = getView().findViewById(R.id.timer);
+        resultBox = getView().findViewById(R.id.resultBox);
+        backBtn = getView().findViewById(R.id.back_btn);
+    }
+
+    public void playState() {
+        sideBarBtn.setVisibility(View.GONE);
+        startBtn.setVisibility(View.GONE);
+        title.setVisibility(View.GONE);
+        resultBox.setVisibility(View.GONE);
+
+        textBox.setVisibility(View.VISIBLE);
+        input.setVisibility(View.VISIBLE);
+        timer.setVisibility(View.VISIBLE);
+        backBtn.setVisibility(View.VISIBLE);
+    }
+
+    public void menuState() {
+        textBox.setVisibility(View.GONE);
+        input.setVisibility(View.GONE);
+        timer.setVisibility(View.GONE);
+
+        sideBarBtn.setVisibility(View.VISIBLE);
+        startBtn.setVisibility(View.VISIBLE);
+        title.setVisibility(View.VISIBLE);
+        resultBox.setVisibility(View.VISIBLE);
+        backBtn.setVisibility(View.VISIBLE);
+
+    }
+
+    public void runTimer() {
+        new CountDownTimer(60000, 1000) {
+
+            public void onTick(long ms) {
+                timer.setText(""+ms/1000);
+            }
+
+            public void onFinish() {
+                menuState();
+            }
+        }.start();
+    }
+
+    public void onStartClick(View view) {
+        playState();
+        runTimer();
+    }
+
 }
