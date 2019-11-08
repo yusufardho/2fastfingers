8e759685bb25cef9d61816a700932900afff8b37
yusufardho <yusufardho@gmail.com>
2019-11-09 06:45:57 +0700

    specify the action only when receiving a call

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/yusuftriardho/twofastfingers/tools/CallReceiver.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/yusuftriardho/twofastfingers/tools/CallReceiver.java
@@ -3,11 +3,18 @@ package id.ac.ui.cs.mobileprogramming.yusuftriardho.twofastfingers.tools;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class CallReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.sendBroadcast(new Intent("INCOMING_CALL_DETECTED"));
+        try {
+            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                context.sendBroadcast(new Intent("INCOMING_CALL_DETECTED"));
+            }
+        } catch (Exception ignored) {}
     }
 }
