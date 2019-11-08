2d874824d14c32d68d8a114fe6c9808f7d5715e6
yusufardho <yusufardho@gmail.com>
2019-11-08 22:01:17 +0700

    multi language in db

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/yusuftriardho/twofastfingers/data/db/entity/Word.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/yusuftriardho/twofastfingers/data/db/entity/Word.java
@@ -5,21 +5,38 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Locale;
+
 @Entity(tableName = "word_table")
 public class Word {
 
     @PrimaryKey
     @NonNull
-    @ColumnInfo(name = "word")
-    private String mWord;
+    @ColumnInfo(name = "word_english")
+    private String word_english;
 
-    public Word(@NonNull String word) {
-        this.mWord = word;
+    @NonNull
+    @ColumnInfo(name = "word_indonesia")
+    private String word_indonesia;
+
+    public Word(@NonNull String word_english, @NonNull String word_indonesia) {
+        this.word_english = word_english;
+        this.word_indonesia = word_indonesia;
     }
 
-    public String getWord(){
-        return this.mWord;
+    public String getWord() {
+        if (Locale.getDefault().getDisplayLanguage().equals("Indonesia")) {
+            return this.word_indonesia;
+        } else {
+            return this.word_english;
+        }
     }
 
-    public void setWord(String word) { this.mWord = word; }
+    public String getWord_english() {
+        return this.word_english;
+    }
+
+    public String getWord_indonesia() {
+        return this.word_indonesia;
+    }
 }
